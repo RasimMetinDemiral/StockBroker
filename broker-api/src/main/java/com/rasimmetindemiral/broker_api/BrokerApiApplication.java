fb29b5da6ff76ba6dad01a7a5120afa98d904417
Rasim Metin Demiral <metindemiral28@hotmail.com>
2025-04-10 12:43:54 +0300

    project resource is added.

---
+++ broker-api/src/main/java/com/rasimmetindemiral/broker_api/BrokerApiApplication.java
@@ -0,0 +1,13 @@
+package com.rasimmetindemiral.broker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerApiApplication.class, args);
+	}
+
+}
---
+++ broker-api/src/test/java/com/rasimmetindemiral/broker_api/BrokerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasimmetindemiral.broker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
