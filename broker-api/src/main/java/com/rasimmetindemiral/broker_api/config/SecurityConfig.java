ad897911c33fdfc44e4340895645cf9c2745ce0b
Rasim Metin Demiral <metindemiral28@hotmail.com>
2025-04-12 00:07:14 +0300

    swagger-ui configuration is added.

--- broker-api/src/main/java/com/rasimmetindemiral/broker_api/config/SecurityConfig.java
+++ broker-api/src/main/java/com/rasimmetindemiral/broker_api/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/customer/**").hasRole("CUSTOMER")
                         .anyRequest().authenticated())
